6d98fbd06a9ce510c66d0b2062f2e007067bc01e
Jaka Hudoklin <jakahudoklin@gmail.com>
2016-07-29 01:55:18 +0200

    pass everything after first arg to docker-compose exec and run

--- default.nix
+++ default.nix
@@ -39,13 +39,13 @@ let
   runEnv = pkgs.writeScriptBin "${config.name}-run" ''
     #!${pkgs.bash}/bin/bash
 
-    ${pkgs.pythonPackages.docker_compose}/bin/docker-compose -p ${config.name} -f ${compose} run $1 $2
+    ${pkgs.pythonPackages.docker_compose}/bin/docker-compose -p ${config.name} -f ${compose} run $1 ''${@:2}
   '';
 
   execEnv = pkgs.writeScriptBin "${config.name}-exec" ''
     #!${pkgs.bash}/bin/bash
 
-    ${pkgs.docker}/bin/docker exec -ti ${config.name}_$1_1 $2
+    ${pkgs.docker}/bin/docker exec -ti ${config.name}_$1_1 ''${@:2}
   '';
 
   attachEnv = pkgs.writeScriptBin "${config.name}-attach" ''
